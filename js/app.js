b10dfb6e3345f5581845fbfaea56910f01837732
obamwonyi <developer.oduwa@gmail.com>
2022-06-24 16:35:47 +0100

    added carousel section to the project

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ let bannerButton_div = document.querySelector(".banner_nav_button");
 
 
 
+//event listener for the banner (note event delegation was implemented , element were not targeted directly) 
 bannerButton_div.addEventListener("click", e => 
 {
     e.preventDefault();
@@ -29,3 +30,20 @@ bannerButton_div.addEventListener("click", e =>
         banner_div.classList.add("banner_div");
     }
 })
+
+
+/* javascript for the carousel will go here ------------------__*/ 
+
+
+function scrollL() 
+{
+    let left = document.querySelector(".scroll_images");
+    left.scrollBy(350,0);
+}
+
+function scrollR() 
+{
+    let right = document.querySelector(".scroll_images");
+    right.scrollBy(-350,0);
+}
+
