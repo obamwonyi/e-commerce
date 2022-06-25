dbd27d3bbd2b321dba4d5fb184bf7ebd1809cbe6
obamwonyi <developer.oduwa@gmail.com>
2022-06-25 13:27:48 +0100

    scrolling section was added and properly styled .

--- js/app.js
+++ js/app.js
@@ -13,37 +13,83 @@ let bannerButton_div = document.querySelector(".banner_nav_button");
 bannerButton_div.addEventListener("click", e => 
 {
     e.preventDefault();
+
+    let buttonOne = document.querySelector("#button1");
+    let buttonTwo = document.querySelector("#button2");
+    let buttonThree = document.querySelector("#button3");
+
     if(e.target.id === "button1") 
     {
+
+                //removing the black  banner navigation button color when not active and setting it to the default value 
+                buttonTwo.classList.remove("banner_button");
+                buttonTwo.classList.add("banner_button_off");
+        
+                //removing the black  banner navigation button color when not active and setting it to the default value
+                buttonThree.classList.remove("banner_button");
+                buttonThree.classList.add("banner_button_off");
+        
+                //removing the default banner button and setting the the active banner 
+                buttonOne.classList.remove("banner_button_off");
+                buttonOne.classList.add("banner_button");
+
+
         banner_div.classList.remove("banner_div_two");
         banner_div.classList.add("banner_div");
     }
     else if( e.target.id === "button2") 
     {
 
+        //selecting button one and two from the banner view selection 
+
+
+        //removing the black  banner navigation button color when not active and setting it to the default value 
+        buttonOne.classList.remove("banner_button");
+        buttonOne.classList.add("banner_button_off");
+
+        //removing the black  banner navigation button color when not active and setting it to the default value
+        buttonThree.classList.remove("banner_button");
+        buttonThree.classList.add("banner_button_off");
+
+        //removing the default banner button and setting the the active banner 
+        buttonTwo.classList.remove("banner_button_off");
+        buttonTwo.classList.add("banner_button");
+
         banner_div.classList.remove("banner_div");
         banner_div.classList.add("banner_div_two");
     }
     else 
     {
+
+
+        buttonTwo.classList.remove("banner_button");
+        buttonTwo.classList.add("banner_button_off");
+
+        //removing the black  banner navigation button color when not active and setting it to the default value
+        buttonOne.classList.remove("banner_button");
+        buttonOne.classList.add("banner_button_off");
+
+        //removing the default banner button and setting the the active banner 
+        buttonThree.classList.remove("banner_button_off");
+        buttonThree.classList.add("banner_button");
+
         banner_div.classList.remove("banner_div_two");
         banner_div.classList.add("banner_div");
     }
 })
 
 
-/* javascript for the carousel will go here ------------------__*/ 
-
+/* javascript for the scroll menu will go here ------------------__*/ 
 
 function scrollL() 
 {
-    let left = document.querySelector(".scroll_images");
+    let left = document.querySelector(".main_scroller");
     left.scrollBy(350,0);
 }
 
 function scrollR() 
 {
-    let right = document.querySelector(".scroll_images");
+    let right = document.querySelector(".main_scroller");
     right.scrollBy(-350,0);
 }
 
