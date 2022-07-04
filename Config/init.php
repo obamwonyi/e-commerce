4651abbdb1218d2261396a20bf6e789a5001a104
obamwonyi <developer.oduwa@gmail.com>
2022-07-04 16:07:43 +0100

    added the config folder and init.php file for the environment initials

---
+++ Config/init.php
@@ -0,0 +1,20 @@
+<?php 
+
+//mysql host name 
+define("DBHost","localhost");
+
+//mysql database name 
+define("DBName","e_commerce");
+
+//mysql user name 
+define("DBUser","admin");
+
+//mysql password 
+define("DBPassword","666365356AdiN");
+
+
+//requiring the autoloader for loading all the classes 
+require_once __DIR__ . "../vendor/autoload.php";
+
+
+?>
