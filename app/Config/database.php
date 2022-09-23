61a7cd6edf3d38fe619b5ae9fbe48232738c8ef9
obamwonyi <developer.oduwa@gmail.com>
2022-09-23 06:44:04 +0100

    fixed the database setting and error for connection failure

--- app/Config/database.php
+++ app/Config/database.php
@@ -6,7 +6,7 @@ return
     "type" => "mysql", 
     "host" => "us-cdbr-east-06.cleardb.net", 
     "port" => "3306",
-    "database" => "e_commerce",
+    "database" => "heroku_ac66e6dee0cb2ba",
     "username" => "b3dabb3090432d",
     "password" => 'e560a15b',
 ];
